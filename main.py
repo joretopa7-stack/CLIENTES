ebc9b477c109c29c799ea576adc6956f8056b82e
joretopa7-stack <joretopa7@gmail.com>
2026-06-10 17:59:11 -0500

    Separacion de modelos, y el id para la creacion

--- main.py
+++ main.py
@@ -1,7 +1,28 @@
 from fastapi import FastAPI
+from modelos.clientes import Cliente, ClienteCrear
 
 app = FastAPI()
 
-@app.get("/")
-def inicio():
-    return {"message": "¡Hola estoy aprendiendo FastAPI!"}
+ListaClientes: list[Cliente] = []
+
+@app.get("/clientes", response_model=list[Cliente])
+def ListarClientes():
+    return ListaClientes
+
+@app.get("/clientes/{cliente_id}", response_model=Cliente)
+def ListarCliente(cliente_id: int):
+
+    for cliente in ListaClientes:
+        if cliente.id == cliente_id:
+            return cliente
+
+    return {"error": "Cliente no encontrado"}
+
+@app.post("/clientes", response_model=Cliente)
+def AgregarCliente(datos_cliente: ClienteCrear):
+
+    ClienteValidado = Cliente.model_validate(datos_cliente.model_dump())
+    
+    ListaClientes.append(ClienteValidado)
+
+    return ClienteValidado
